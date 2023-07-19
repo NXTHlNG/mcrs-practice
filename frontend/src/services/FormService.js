0fdfc14b16589d7ba53407735d39519f5c3417f7
Artem Zhavoronkov <azhavoronkov@aeroidea.ru>
2023-07-19 12:12:35 +0400

    fix nginx and docker configs

--- frontend/src/services/FormService.js
+++ frontend/src/services/FormService.js
@@ -1,56 +1,55 @@
 import axios from "axios";
 
+let baseURL;
+
+if (window.location.origin === "http://localhost:3000") {
+    baseURL = "http://127.0.0.1:8000";
+} else {
+    baseURL = window.location.origin;
+}
+
+baseURL = baseURL + "/api";
+
+axios.defaults.auth = {
+    username: process.env.REACT_APP_BASIC_AUTH_USER,
+    password: process.env.REACT_APP_BASIC_AUTH_PASSWORD,
+};
+
 export const FormService = {
     async getAll() {
-        const res = await axios.get(`${process.env.REACT_APP_API_URL}/form`);
+        const res = await axios.get(`${baseURL}/form`);
         return res.data;
     },
 
     async get(alias) {
-        const res = await axios.get(
-            `${process.env.REACT_APP_API_URL}/form/${alias}`
-        );
+        const res = await axios.get(`${baseURL}/form/${alias}`);
         return res.data;
     },
 
     async delete(alias) {
-        const res = await axios.delete(
-            `${process.env.REACT_APP_API_URL}/form/${alias}`
-        );
+        const res = await axios.delete(`${baseURL}/form/${alias}`);
         return res.data;
     },
 
     async create(form) {
-        const res = await axios.post(
-            `${process.env.REACT_APP_API_URL}/form`,
-            form
-        );
+        const res = await axios.post(`${baseURL}/form`, form);
         return res.data;
     },
 
     async update(form) {
-        const res = await axios.put(
-            `${process.env.REACT_APP_API_URL}/form/${form.alias}`,
-            form
-        );
+        const res = await axios.put(`${baseURL}/form/${form.alias}`, form);
         return res.data;
     },
 
     async answer(data) {
-        const res = await axios.post(
-            `${process.env.REACT_APP_API_URL}/answer`,
-            data
-        );
+        const res = await axios.post(`${baseURL}/answer`, data);
         return res.data;
     },
 
     async download(alias) {
-        const res = await axios.get(
-            `${process.env.REACT_APP_API_URL}/download/${alias}`,
-            {
-                responseType: "blob",
-            }
-        );
+        const res = await axios.get(`${baseURL}/download/${alias}`, {
+            responseType: "blob",
+        });
         const href = window.URL.createObjectURL(res.data);
 
         const anchorElement = document.createElement("a");
