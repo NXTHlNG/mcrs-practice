69682154f5e388ef49b56d1e7ac9613c4f969db3
Artem Zhavoronkov <zhavoronkov@aeroidea.ru>
2023-10-03 17:57:41 +0400

    added notfound and index pages

--- frontend/src/components/IndexPage/IndexPage.js
+++ frontend/src/components/IndexPage/IndexPage.js
@@ -1,15 +1,12 @@
 import { Link, Container, Stack, Typography } from "@mui/material"
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 
 const IndexPage = () => {
-    const navigate = useNavigate();
-    useEffect(() => navigate(`/admin`))
+    window.location.href = "admin"
 
     return <>
         <Container sx={{ height: "100vh" }}>
             <Stack height={"100%"} justifyItems={"center"} justifyContent={"center"} alignItems={"center"}>
-                <Typography>Перенаправляю на <Link component="button" onClick={() => navigate(`/admin`)}>админ панель</Link></Typography>
+                <Typography>Перенаправляю на <Link component="button" onClick={() => window.location.href = "admin"}>админ панель</Link></Typography>
             </Stack>
         </Container>
     </>;
